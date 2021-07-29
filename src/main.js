8705b0a8dac52d5edc296d0570a8d58ecd3d2e8a
Anna <anna.chowattanakul@gmail.com>
2021-07-29 01:48:01 -0400

    Fix build error

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
-import AppLink from "@/components/Applink.vue";
+import AppLink from "./components/Applink.vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
